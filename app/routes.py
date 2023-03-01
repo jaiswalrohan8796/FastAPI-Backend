72216d199a5cd2c96226694d077036d54885a39a
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2023-03-02 01:54:36 +0530

    added authorization using jwt

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,27 @@
-from fastapi import APIRouter, Request, Form
+from fastapi import APIRouter, Request, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from models.user import UserSchema
-from utils.utils import validate_signup_data, hash_password, check_password, make_new_user_dict, validate_login_data
+from utils.utils import validate_signup_data, hash_password, check_password, make_new_user_dict, validate_login_data, create_access_token
 from database.mongodb import insert_user, find_user_with_email
+from app.auth import get_current_user
 # app configurations
 templates = Jinja2Templates(directory="templates")
 api_router = APIRouter()
 
 
-# =========== GET Routes ==============
-
-# Home Page (Login)
-@api_router.get("/app", tags=["Home"], response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("app.html", {"request": request, "errors": []})
-# Signup Page
+# ===================== Un-protected routes =========================
 
 
 @api_router.get("/signup", tags=["Signup"], response_class=HTMLResponse)
 async def signup(request: Request):
     return templates.TemplateResponse("signup.html", {"request": request, "errors": []})
 
-# Login
-
 
 @api_router.get("/", tags=["Login"], response_class=HTMLResponse)
 async def login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "errors": []})
-
-# =============== POST Routes ==========================
 
 
 @api_router.post("/signup")
@@ -61,27 +53,35 @@ async def handle_signup(request: Request):
 
 
 @api_router.post("/login")
-async def handler_login(request: Request):
-    # extract data
+async def handle_login(request: Request):
     form_data = await request.form()
-    # validate data
+    email = form_data["email"]
+    password = form_data["password"]
+    # validate login data
     errors = validate_login_data(form_data)
     if len(errors) > 0:
         return templates.TemplateResponse("login.html", {"request": request, "errors": errors})
-    # extract data
-    email = form_data["email"]
-    password = form_data["password"]
-    # check user exists
-    user_exists = await find_user_with_email(email)
-    if user_exists is None:
-        return templates.TemplateResponse("login.html", {"request": request, "errors": ["User doesn't exist. Please login."]})
-    print(user_exists)
-    # check password
-    is_password_matched = check_password(password, user_exists["password"])
-    print(is_password_matched)
+    user = await find_user_with_email(email)
+    if user == None:
+        return templates.TemplateResponse("login.html", {"request": request, "errors": ["User doesn't exist."]})
+    is_password_matched = check_password(password, user["password"])
     if is_password_matched == False:
         return templates.TemplateResponse(
             "login.html", {"request": request, "errors": ["Password incorrect."]})
-    else:
-        print("Logged in")
-        return templates.TemplateResponse("app.html", {"request": request, "errors": []})
+    access_token = create_access_token(email)
+    response = templates.TemplateResponse(
+        "app.html", {"request": request, "user": user,  "token": access_token})
+    response.set_cookie("token", access_token)
+    return response
+
+
+# ============== Protected Routes ==============
+
+@api_router.get("/app")
+async def handle_app(request: Request, user=Depends(get_current_user)):
+    if user is None:
+        return templates.TemplateResponse("login.html", {"request": request, "errors": ["Authentication failed. Login"]})
+    response = templates.TemplateResponse(
+        "app.html", {"request": request, "user": user["user"],  "token": user["token"]})
+    response.set_cookie("token", user["token"])
+    return response
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,12 @@
 from validate_email import validate_email
+from dotenv import dotenv_values
 import bcrypt
+import os
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
+
+config = dotenv_values()
 
 
 def validate_signup_data(data):
@@ -46,3 +53,15 @@ def make_new_user_dict(username, email, hashed_password):
         "password": hashed_password,
         "todos": []
     }
+
+
+def create_access_token(subject: Union[str, Any]) -> str:
+    token_expire_mins = config["JWT_ACCESS_TOKEN_EXPIRE_MINUTES"]
+    expires_delta = float(
+        token_expire_mins) if token_expire_mins is not None else 30
+    to_encode = {"exp": datetime.utcnow(
+    ) + timedelta(minutes=expires_delta), "sub": str(subject)}
+    encoded_jwt = jwt.encode(
+        to_encode, config["JWT_SECRET_KEY"], config["JWT_ALGORITHM"])
+    return encoded_jwt
+
